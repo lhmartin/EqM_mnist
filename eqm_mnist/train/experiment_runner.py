a4f43abee9d5d9dd1fafa445b6aeacf9a57331fb
Luke Martin <lukechile@gmail.com>
2025-11-02 13:56:42 -0500

    save img

--- eqm_mnist/train/experiment_runner.py
+++ eqm_mnist/train/experiment_runner.py
@@ -12,6 +12,7 @@ import torch
 import yaml
 import os
 import numpy as np
+import matplotlib.pyplot as plt
 
 P_SIMPLE_SHAPE = [1, 32, 32]
 
@@ -33,6 +34,13 @@ class ExperimentRunner:
         num_residual_layers: int = 2
         t_embed_dim: int = 40
         y_embed_dim: int = 40
+        # inference settings
+        step_sizes: list[float] = [0.01, 0.02, 0.05, 0.1]
+        nag_factors: list[float] = [0.1, 0.2, 0.4, 0.8]
+        g_thresholds: list[int] = [10, 20, 50, 100]
+        max_steps: list[int] = [50, 100, 200, 500]
+        guidance_scales: list[float] = [0.5, 1.0, 5.0, 10.0, 100.0]
+
         
     def __init__(self, config: 'Config'):
         self.config = config
@@ -62,17 +70,7 @@ class ExperimentRunner:
                                     lr=self.config.lr, 
                                     batch_size=self.config.batch_size)
         
-        # Sample from the model
-        sampler = EqMSampler(self.model, device=self.device)
-        initial_samples = self.data_sampler.p_simple.sample(50)[0].to(self.device)
-        samples, intermediate_steps = sampler.sample(initial_samples=initial_samples, 
-                                 step_size=0.01, 
-                                 nag_factor=0.4, 
-                                 g_threshold=20,
-                                 max_steps=200, 
-                                 target_y=5, 
-                                 guidance_scale=5.0,
-                                 verbose=True)
+        self.run_sampling()
         
         # save the samples
         os.makedirs(save_dir, exist_ok=True)
@@ -95,6 +93,50 @@ class ExperimentRunner:
         with open(os.path.join(save_dir, "config.yaml"), "w") as f:
             yaml.dump(self.config.model_dump(), f)
         print(f"Config saved to {os.path.join(save_dir, "config.yaml")}")
+        
+    def get_inference_settings(self):
+        """
+        Returns a list of inference settings.
+        """
+        for step_size in self.config.step_sizes:
+            for nag_factor in self.config.nag_factors:
+                for g_threshold in self.config.g_thresholds:
+                    for max_steps in self.config.max_steps:
+                        for guidance_scale in self.config.guidance_scales:
+                            yield {
+                                "step_size": step_size,
+                                "nag_factor": nag_factor,
+                                "g_threshold": g_threshold,
+                                "max_steps": max_steps,
+                                "guidance_scale": guidance_scale
+                            }
+
+    def run_sampling(self, save_dir: str):
+        """
+        Samples from the trained model.
+        Gets a batch of 50 samples of each digit, and an unguided sample.
+        
+        Loops through the inference settings defined in the config.
+        Saves the outputs as images, video, and numpy arrays.
+        
+        """
+                # Sample from the model
+        sampler = EqMSampler(self.model, device=self.device)
+        initial_samples = self.data_sampler.p_simple.sample(50)[0].to(self.device)
+        for target_y in range(10):
+            for inference_settings in self.get_inference_settings():
+                samples, intermediate_steps = sampler.sample(initial_samples=initial_samples, 
+                                                             **inference_settings,
+                                                             target_y=target_y,
+                                                             verbose=True)
+                # save the samples
+                inference_settings_str = "_".join([f"{k}={v}" for k, v in inference_settings.items()])s
+                os.makedirs(save_dir, exist_ok=True)
+                plt.imsave(os.path.join(save_dir, f"samples_{target_y}_{inference_settings_str}.png"), samples.detach().cpu().numpy())
+                np.save(os.path.join(save_dir, f"samples_{target_y}_{inference_settings_str}.npy"), samples.detach().cpu().numpy())
+                np.save(os.path.join(save_dir, f"intermediate_steps_{target_y}_{inference_settings_str}.npy"), intermediate_steps.detach().cpu().numpy())
+                print(f"Samples saved to {os.path.join(save_dir, f'samples_{target_y}_{inference_settings_str}.npy')}")
+                print(f"Intermediate steps saved to {os.path.join(save_dir, f'intermediate_steps_{target_y}_{inference_settings_str}.npy')}")
 
 
 def get_c_function(config: ExperimentRunner.Config) -> Callable:
